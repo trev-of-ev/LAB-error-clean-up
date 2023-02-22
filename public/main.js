205d72678c6eeffbb243ef91e0fc865efbd34864
Trevor Guinn <tdguinn87@gmail.com>
2023-02-21 18:35:34 -0600

    Modularize the data

--- public/main.js
+++ public/main.js
@@ -1,31 +1,6 @@
 // import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
 import '../styles/main.scss';
 
-const students = [];
-const voldysArmy = []; // starts as an empty array
-const houses = [
-  {
-    house: 'gryffindor',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png'
-  },
-  {
-    house: 'slytherin',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png'
-  },
-  {
-    house: 'hufflepuff',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png'
-  },
-  {
-    house: 'ravenclaw',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png'
-  }
-];
-
 const renderToDOM = (divId, content) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = content;
